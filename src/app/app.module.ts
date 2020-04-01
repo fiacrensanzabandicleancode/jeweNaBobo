742744b8ea49db21b79269efe41ea2a8e7058f1b
NSANZABANDI Fiacre <fiacre.nsanzabandi@soprasteria.com>
2020-04-01 19:06:07 +0200

    Did some clean.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
 import { UserInformationComponent } from './component/user-information/user-information.component';
-import { from } from 'rxjs';
+
+
  
 @NgModule({
   declarations: [
